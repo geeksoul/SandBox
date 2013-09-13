03c8f1290f7ced256a5f0404f212e3a11311c808
geeksoul <geeksoul@gmail.com>
2013-09-13 21:38:47 +0800

    科~
    哈哈詳細

---
+++ List_1-14.cpp
@@ -0,0 +1,19 @@
+// 
+// 讀入名字後問好
+//
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << " 你的名字是？";
+    cin >> name ;
+
+    cout << " 你娘腳好! " << name << " 先生。\n ";
+
+    return 0;
+}
